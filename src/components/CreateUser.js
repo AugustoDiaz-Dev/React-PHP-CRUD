c7ae2b6fa7a38e2cee2fe405bf7cef673246eda9
Augusto <augustodiaz_1@hotmail.com>
2022-09-09 01:29:36 +1000

    Create form

--- src/components/CreateUser.js
+++ src/components/CreateUser.js
@@ -1,9 +1,39 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 const CreateUser = () => {
+
+  const [inputs, setInputs] = useState({});
+
+    const handleChange = event => {
+      const name = event.target.name;
+      const value = event.target.value;
+      setInputs(values => ({
+        ...values, [name]: value
+      }))
+    }
+
+    const handleSubmit = event => {
+      event.preventDefault();
+
+      console.log(inputs);
+
+    }
+
   return (
     <div>
         <h2>Create User</h2>
+        <form onSubmit={handleSubmit}>
+          <label htmlFor="">Name: </label>
+          <input type="text" name='name' onChange={handleChange} placeholder='Name' />
+
+          <label htmlFor="">Email: </label>
+          <input type="email" name='email' onChange={handleChange} placeholder='Email'/>
+
+          <label htmlFor="">Phone: </label>
+          <input type="text" name='mobile' onChange={handleChange} placeholder='Phone number'/>
+
+          <button>Save</button>
+        </form>
     </div>
   )
 }
